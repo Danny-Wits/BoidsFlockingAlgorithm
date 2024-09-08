394c32f99814712c4f0d032118c41d4178d62625
Danishwer _ <dannywits12321@gmail.com>
2024-09-09 00:54:30 +0530

    added other rules

--- Bird.js
+++ Bird.js
@@ -1,13 +1,13 @@
 class Bird {
   constructor() {
     this.position = createVector(random(width), random(height));
-    this.velocity = createVector(random(-1, 1) * 3, random(-1, 1) * 3);
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
     this.acceleration = createVector();
   }
 
   show() {
     strokeWeight(10);
-    stroke(0xfff);
+    stroke(122, 255, 122);
     point(this.position.x, this.position.y);
   }
   movement() {
@@ -15,18 +15,81 @@ class Bird {
     this.velocity.add(this.acceleration);
   }
   alignment(birds) {
-    const perceptionRadius = 50;
+    let perceptionRadius = alignmentRadius.value();
     let total = 0;
     let steering = createVector();
     for (const bird of birds) {
       let distance = this.position.dist(bird.position);
-      if (distance < perceptionRadius) {
+      if (this !== bird && distance < perceptionRadius) {
         steering.add(bird.velocity);
         total++;
       }
     }
     if (total > 0) {
-      this.acceleration = steering.div(total).sub(this.velocity);
+      this.acceleration.add(
+        steering
+          .div(total)
+          .sub(this.velocity)
+          .setMag(1)
+          .mult(alignmentSpeed.value())
+          .limit(10)
+      );
+    }
+  }
+  cohesion(birds) {
+    let perceptionRadius = cohesionRadius.value();
+    let total = 0;
+    let steering = createVector();
+    for (const bird of birds) {
+      let distance = this.position.dist(bird.position);
+      if (this !== bird && distance < perceptionRadius) {
+        steering.add(bird.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      this.acceleration.add(
+        steering
+          .div(total)
+          .sub(this.position)
+          .setMag(0.1)
+          .mult(cohesionSpeed.value())
+          .limit(2)
+      );
+    }
+  }
+  separation(birds) {
+    const perceptionRadius = separationRadius.value();
+    let total = 0;
+    let steering = createVector();
+    for (const bird of birds) {
+      let distance = this.position.dist(bird.position);
+      if (this !== bird && distance < perceptionRadius) {
+        let vec = p5.Vector.sub(this.position, bird.position);
+        steering.add(vec.div(distance));
+        total++;
+      }
+    }
+    if (total > 0) {
+      this.acceleration.add(
+        steering
+          .div(total)
+          .sub(this.velocity)
+          .setMag(0.1)
+          .mult(separationSpeed.value() * 2)
+          .limit(10)
+      );
+    }
+  }
+  wall() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x <= 0) {
+      this.position.x = width;
+    } else if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y <= 0) {
+      this.position.y = height;
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,19 +1,64 @@
-const birds = [];
-const total = 100;
+let birds = [];
+let total = 100;
+let alignmentRadius;
+let cohesionRadius;
+let separationRadius;
+let alignmentSpeed;
+let cohesionSpeed;
+let separationSpeed;
+let ar;
+let cr;
+let sr;
+let as;
+let cs;
+let ss;
+let tb;
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight - 50);
+  ar = createDiv();
+  alignmentRadius = createSlider(0, 1000, 200);
+  cr = createDiv("Cohesion Radius");
+  cohesionRadius = createSlider(0, 1000, 50);
+  sr = createDiv("Separation Radius  ");
+  separationRadius = createSlider(0, 100, 20);
+  as = createDiv("Alignment Force");
+  alignmentSpeed = createSlider(0, 20, 8, 0.1);
+  cs = createDiv("Cohesion Force  ");
+  cohesionSpeed = createSlider(0, 1, 0.1, 0.1);
+  ss = createDiv("Separation Force  ");
+  separationSpeed = createSlider(0, 10, 0.4, 0.1);
+  tb = createDiv("Total  Birds  ");
+  totalBirds = createSlider(0, 1000, 100);
+  reset = createButton("RESET");
+  reset.mousePressed(() => {
+    birds = [];
+    for (let index = 0; index < totalBirds.value(); index++) {
+      birds.push(new Bird());
+    }
+  });
   for (let index = 0; index < total; index++) {
     birds.push(new Bird());
   }
 }
 window.addEventListener("resize", () => {
-  resizeCanvas(window.innerWidth, window.innerHeight);
+  resizeCanvas(window.innerWidth, window.innerHeight - 50);
 });
 function draw() {
   background(0x000);
+  ar.html("Alignment Radius:" + alignmentRadius.value());
+  cr.html("Cohesion Radius:" + cohesionRadius.value());
+  sr.html("Separation Radius:" + separationRadius.value());
+  as.html("Alignment Force:" + alignmentSpeed.value());
+  cs.html("Cohesion Force:" + cohesionSpeed.value());
+  ss.html("Separation Force:" + separationSpeed.value());
+  tb.html("Total birds:" + totalBirds.value());
   for (const bird of birds) {
+    bird.acceleration.set(0, 0);
     bird.alignment(birds);
+    bird.cohesion(birds);
+    bird.separation(birds);
     bird.movement();
+    bird.wall();
     bird.show();
   }
 }
